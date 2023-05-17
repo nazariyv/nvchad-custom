9d41c32c7aea8fc0b089c75a071b3ebbac8a69c9
Nazariy Vavryk <hi@naz.ooo>
2023-05-17 18:16:56 +0100

    chore: formatter for solidity files

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -28,11 +28,25 @@ lspconfig.rust_analyzer.setup {
   },
 }
 
+
+-- Solidity LSPConfig will be weird until nomic foundation sls is merged into nvim-lspconfig
+local utils = require("core.utils")
+local solidity_on_attach = function(client, bufnr)
+  -- this changes the original on attach to allow document formatting
+  utils.load_mappings("lspconfig", { buffer = bufnr })
+  if client.server_capabilities.signatureHelpProvider then
+    require("nvchad_ui.signature").setup(client)
+  end
+  if not utils.load_config().ui.lsp_semantic_tokens then
+    client.server_capabilities.semanticTokensProvider = nil
+  end
+end
+
 lspconfig.solidity.setup {
   -- !!! ------------- !!!
   -- if you miss adding the on_attach and capabilties here, lsp features like
   -- go to definition, or inspecting diagnostic messages will **NOT** work!!
-  on_attach = on_attach,
+  on_attach = solidity_on_attach,
   capabilities = capabilities,
   -- !!! ------------- !!!
   cmd = { "nomicfoundation-solidity-language-server", "--stdio" },
