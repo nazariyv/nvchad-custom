0e79666ea9b14a711f355f5760cc2639c7717985
Nazariy Vavryk <hi@naz.ooo>
2023-10-09 13:42:47 +0100

    chore: statusline change

--- chadrc.lua
+++ chadrc.lua
@@ -6,7 +6,7 @@ local highlights = require "custom.highlights"
 
 M.ui = {
   theme = "gruvbox",
-  theme_toggle = { "gruvbox", "one_light" },
+  theme_toggle = { "gruvbox", "gruvbox_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
@@ -14,7 +14,11 @@ M.ui = {
   telescope = { style = "bordered" },
 
   statusline = {
-    theme = "vscode_colored",
+    theme = "default", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "block",
+    overriden_modules = nil,
   },
 
   nvdash = {
