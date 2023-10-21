32873c2dee7abcc32bc4a3f7f4e96b64dffde46c
naz <hi@naz.ooo>
2023-10-21 15:26:41 +0100

    feat: primeagen vim options

--- init.lua
+++ init.lua
@@ -5,3 +5,37 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+local vo = vim.opt
+
+vo.guicursor = ""
+
+vo.nu = true
+vo.relativenumber = true
+
+vo.tabstop = 4
+vo.softtabstop = 4
+vo.shiftwidth = 4
+vo.expandtab = true
+
+vo.smartindent = true
+
+vo.wrap = false
+
+vo.swapfile = false
+vo.backup = false
+vo.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vo.undofile = true
+
+vo.hlsearch = false
+vo.incsearch = true
+
+vo.termguicolors = true
+
+vo.scrolloff = 8
+vo.signcolumn = "yes"
+vo.isfname:append("@-@")
+
+vo.updatetime = 50
+
+vo.colorcolumn = "80"
